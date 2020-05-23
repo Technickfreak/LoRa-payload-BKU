7e7e0f6a114d3a05294847f107a4da7875481495
Technickfreak <Technickfreak@users.noreply.github.com>
2020-05-23 12:28:32 +0200

    VSC_MKR_1300_Test.ino überarbeitet

--- examples/VSC_MKR_1300_Test.ino
+++ examples/VSC_MKR_1300_Test.ino
@@ -1,22 +1,20 @@
-
-//#include "LoRa-payload-BKU.h"
-#include <LoRa-payload-BKU.h>
-//#include "\..\src\LoRa-payload-BKU.h"
-//#include "D:\Schule\VS-Cdode\VSC MKR 1300\LoRa_mkr_wan_Lib\LoRa_mkr_wan_Lib.h"
+//#include "LoRa_payload_BKU.h"
+//#include <LoRa_payload_BKU.h>
+#include <D:\Schule\VS-Cdode\VSC MKR 1300\LoRa_mkr_wan_Lib\LoRa_payload_BKU.h>
 
 //use your secrets.h file or enter appEui & appKey below
-//  #include secrets.h
+#include <c:\secrets\secrets.h>
+String appEui = xxxxxxxxxxxxxxxx;
+String appKey = xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
 
-    String appEui = "****************";
-    String appKey = "********************************";
-    
 //set your frequenze plan values are: (EU868,AS923,AU915,KR920,IN865,US915,US915_HYBRID)
-    _lora_band frequency = EU868;
+_lora_band frequency = EU868;
+
 
 void setup()
 {
     Serial.begin(115200);
-    setupLorawan(appEui, appKey, frequency);
+    BkuSetupLorawan(appEui, appKey, frequency);
     
     Serial.print("Your device EUI is: ");
     Serial.println(modem.deviceEUI());
@@ -24,32 +22,32 @@ void setup()
 
 void loop()
 {
-    //fill your payload          Zeilenweises befüllen 
-    payload testpayload[2];
+    //fill your payload
+    BkuPayload testpayload[2];
     testpayload[0].TYPE = TYPE_TEMP;
     testpayload[0].wert = (-15.5);
     testpayload[1].TYPE = TYPE_RH;
     testpayload[1].wert = (27);
     
-    /*                           Blockbefüllung
-    payload testpayload2[] = {
+    /*
+    BkuPayload testpayload2[] = {
                             payload(TYPE_RH, 66),
                             payload(TYPE_TEMP, 20.6),
                             };
     */
-    /*                          Blockbefüllung 
-    payload testpayload3[] = {
+    /*
+    BkuPayload testpayload3[] = {
                             {TYPE_RH, 66},
                             {TYPE_TEMP, 20.6},
                             };
     */
 
 
-    sendPayload(testpayload);
+    BkuSendPayload(testpayload);
     delay(1000);    
-    uint8_t *MSG = receiveData();
+    uint8_t *MSG = BkuReceiveData();
     if (MSG[0] = 0){Serial.println("No downlink message received at this time.");} 
-    printMSG(MSG);
+    BkuPrintMSG(MSG);
 
     delay(22000);
 
